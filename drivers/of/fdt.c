4eee2218167395f492a5dcdc805480e92565daa9
Quentin Perret <qperret@google.com>
2021-05-12 12:30:38 +0000

    Revert "fdt: Properly handle "no-map" field in the memory region"
    This reverts commit 86ac82a7c708acf4738c396228be7b8fdaae4d99.
    It is not really a fix, and the backport misses dependencies, which
    breaks existing platforms.
    
    Reported-by: Alexandre TORGUE <alexandre.torgue@foss.st.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1205,8 +1205,8 @@ int __init __weak early_init_dt_reserve_memory_arch(phys_addr_t base,
 {
 
 	if (nomap)
-		return memblock_mark_nomap(base, size);
-
+		return memblock_remove(base, size);
+		
 	return memblock_reserve(base, size);
 }
 
